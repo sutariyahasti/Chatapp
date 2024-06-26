c77ca4ad5c9153a36d98b4c602be28c4baae0c30
sutariyahasti <hastisutariya01@gmail.com>
2024-06-26 17:00:44 +0530

    Update page.js
    login page added to first display

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,10 @@
-"use client"
-import Image from "next/image";
-import { useEffect, useState } from "react";
+"use client";
+import Login from "./pages/Login/page";
 
 export default function Home() {
-  const [users, setUsers] = useState([]);
-console.log(users);
-  useEffect(() => {
-    fetch('/api/alluser')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => setUsers(data))
-      .catch(error => console.error('Error fetching users:', error));
-  }, []);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-   hello
-      <h1>Users List</h1>
-      <ul>
-        {users.map(user => (
-          <li key={user._id}>{user.name} - {user.email}</li>
-        ))}
-      </ul>
-      <a href="/pages/Login" className="mb-10 block font-bold text-gray-600">
-        Have an Account? Login
-      </a>
-    </main>
+    <>
+      <Login />
+    </>
   );
 }

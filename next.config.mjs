f93a3dd20037e2e0b7978dcb6faa96db8e351cf2
sutariyahasti <hastisutariya01@gmail.com>
2024-06-26 12:23:43 +0530

    vercel.json added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+// next.config.js
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        MONGODB_URI: process.env.MONGODB_URI,
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    },
+};
 
 export default nextConfig;

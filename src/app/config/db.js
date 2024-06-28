d2636b67f90766b6d0d70f1a4934cef60e689b3d
sutariyahasti <hastisutariya01@gmail.com>
2024-06-28 11:53:23 +0530

    Update db.js

--- src/app/config/db.js
+++ src/app/config/db.js
@@ -8,7 +8,7 @@ const uri = process.env.NEXT_MONGODB_URI;
 console.log('MongoDB URI:', uri); // Add logging to check if the URI is being read correctly
 
 const options = {
-    databaseName: 'ChatAppData'
+     dbName: 'ChatAppData'
 };
 
 let client;

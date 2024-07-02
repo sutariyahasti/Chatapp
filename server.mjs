7164c3f643e1a16c4bb877f091b806e6aed0ddf5
sutariyahasti <hastisutariya01@gmail.com>
2024-07-02 15:22:13 +0530

    update code for socket error
    add vercel file

--- server.mjs
+++ server.mjs
@@ -5,7 +5,7 @@ import { Server } from "socket.io";
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
 const port = 3000;
-const app = next({ dev, hostname, port });
+const app = next({ dev });
 const handler = app.getRequestHandler();
 let users = [];
 let chats = [];
